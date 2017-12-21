ad62dc6321b9bd4f4e098ee5c4065e2162e30e52
Alan Wilson <nannerfox@me.com>
2017-12-20 20:23:19 -0600

    Display changes
    Fixed base code from school. Store and pre-render works. Non functional yet. Result render yet to be displayed.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = ({ title }) => (
   <div>
     <h1>{title}</h1>
-    <a href="/">Home</a>
-    <a href="/search">Search</a>
+    <a href="/">Home | </a>
+    <a href="/search">Search | </a>
     <a href="/mine">Mine</a>
   </div>
 );
--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { component } from 'react';
 import { render } from 'react-dom';
-import { Home, Mine, Search } from './screens';
+import Search from './Search';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducEm from "./reducers/reducEm";
 
+// import { Home, Mine, Search } from './screens';
+//
+// const Index = ({ pathname }) => {
+//   switch(pathname) {
+//     case "/search":
+//       return <Search />;
+//     case "/mine":
+//       return <Mine />;
+//     default:
+//       return <Home />;
+//   }
+// };
+//
+// let pathname = window.location.pathname;
+//
+// render(<Index pathname={pathname} />, document.getElementById("root"));
+//
+//  window.addEventListener("popstate", () => {
+//   pathname = window.location.pathname;
+// });
+const store = createStore(reducEm);
 
-const Index = ({ pathname }) => {
-  switch(pathname) {
-    case "/search":
-      return <Search />;
-    case "/mine":
-      return <Mine />;
-    default:
-      return <Home />;
-  }
-};
-
-let pathname = window.location.pathname;
-
-render(<Index pathname={pathname} />, document.getElementById("root"));
-
- window.addEventListener("popstate", () => {
-  pathname = window.location.pathname;
-});
+render(
+  <Provider store={store}>
+  <Search />
+</Provider>,
+  document.getElementById("root")
+);
